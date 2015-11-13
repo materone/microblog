2a86bda122871d684abf65928cd384233f812200
Tony <Tony@TonydeiMac.lan>
2015-11-13 22:46:51 +0000

    Add gotop style

--- app.js
+++ app.js
@@ -67,6 +67,22 @@ app.get('/login',routes.login);
 app.post('/login',routes.doLogin);
 app.get('/logout',routes.logout);
 
+// 一个中间件栈，处理指向 /user/:id 的 GET 请求
+app.get('/duser/:id', function (req, res, next) {
+  // 如果 user id 为 0, 跳到下一个路由
+  if (req.params.id == 0) next('route');
+  // 否则将控制权交给栈中下一个中间件
+  else next(); //
+}, function (req, res, next) {
+  // 渲染常规页面
+  res.send('regular');
+});
+
+// 处理 /user/:id， 渲染一个特殊页面
+app.get('/duser/:id', function (req, res, next) {
+  res.send('special');
+});
+
 app.use('/users', users);
 app.use('/hello',routes);
 app.use('/hello/:foo',routes);
@@ -85,7 +101,7 @@ function checkNotLogin(req,res,next){
     return res.redirect('/');
   }
   next();
-}e
+}
 
 app.get('/file/:name', function (req, res, next) {
   var options = {
